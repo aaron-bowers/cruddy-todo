59ff150073e0ea1772f83f4f0416399133edb318
Aaron Bowers <bowersaaronjames@gmail.com>
2022-05-03 11:43:16 -0700

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -65,14 +65,16 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  let fileName = id + '.txt';
+  fs.unlink(path.join(exports.dataDir, fileName), err => {
+    if (err) {
+      // report an error if item not found
+      callback(new Error(`Nothing to see here at ${id}`));
+    } else {
+      callback();
+    }
+  });
+
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
